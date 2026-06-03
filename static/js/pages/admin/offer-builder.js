88d216736c1d472239a2be6174da3984e24098cb
kkmiecik-coder <konrad.kmiecik@rsholding.com.pl>
2026-06-03 14:14:36 +0200

    feat(offer-builder): searchable dropdown for set variant group

--- static/js/pages/admin/offer-builder.js
+++ static/js/pages/admin/offer-builder.js
@@ -1312,10 +1312,18 @@ function addSetVariantGroup(btn) {
                         <path d="M1 2.5A1.5 1.5 0 0 1 2.5 1h3A1.5 1.5 0 0 1 7 2.5v3A1.5 1.5 0 0 1 5.5 7h-3A1.5 1.5 0 0 1 1 5.5v-3zM2.5 2a.5.5 0 0 0-.5.5v3a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 0-.5-.5h-3zm6.5.5A1.5 1.5 0 0 1 10.5 1h3A1.5 1.5 0 0 1 15 2.5v3A1.5 1.5 0 0 1 13.5 7h-3A1.5 1.5 0 0 1 9 5.5v-3zm1.5-.5a.5.5 0 0 0-.5.5v3a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 0-.5-.5h-3zM1 10.5A1.5 1.5 0 0 1 2.5 9h3A1.5 1.5 0 0 1 7 10.5v3A1.5 1.5 0 0 1 5.5 15h-3A1.5 1.5 0 0 1 1 13.5v-3zm1.5-.5a.5.5 0 0 0-.5.5v3a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 0-.5-.5h-3zm6.5.5A1.5 1.5 0 0 1 10.5 9h3a1.5 1.5 0 0 1 1.5 1.5v3a1.5 1.5 0 0 1-1.5 1.5h-3A1.5 1.5 0 0 1 9 13.5v-3zm1.5-.5a.5.5 0 0 0-.5.5v3a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 0-.5-.5h-3z"/>
                     </svg>
                 </div>
-                <select class="form-select variant-group-select set-item-variant-group" onchange="updateSetVariantGroupProducts(this)">
-                    <option value="">Wybierz grupę...</option>
-                    ${variantGroupOptions}
-                </select>
+                <div class="custom-select-wrapper">
+                    <select class="form-select variant-group-select set-item-variant-group searchable-select" onchange="updateSetVariantGroupProducts(this)">
+                        <option value="">Wybierz grupę...</option>
+                        ${variantGroupOptions}
+                    </select>
+                    <div class="custom-select-display">
+                        <span class="selected-text">Wybierz grupę...</span>
+                        <svg width="12" height="12" viewBox="0 0 16 16" fill="currentColor">
+                            <path d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"/>
+                        </svg>
+                    </div>
+                </div>
                 <span class="set-item-badge badge-variant">Grupa</span>
             </div>
             <div class="variant-group-products">
